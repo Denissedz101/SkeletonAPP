ead5c96bbea8ddb71ec844346318a2c5bf9a6fcc
Denisse Díaz <denissedz@gmail.com>
2025-05-24 17:53:13 -0400

    Primer commit del proyecto SkeletonAPP

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+  standalone: false,
+})
+export class Tab2Page {
+
+    // Variables
+  usuario: string = '';
+  nombre: string = '';
+  apellidos: string = '';
+
+  mostrarMensaje: boolean = true; // mostramos mensaje intro
+  
+  constructor() {}
+
+    // Funciones que se ejecuta al enviar el formulario
+  capturarDatos() {
+    console.log('Usuario:', this.usuario);
+    console.log('Nombre:', this.nombre);
+    console.log('Apellidos:', this.apellidos);
+    
+    // Aquí hacemos las validaciones 
+    this.mostrarMensaje = false; // Ocultamos el mensaje al iniciar sesion
+
+
+
+
+  }
+
+
+}
